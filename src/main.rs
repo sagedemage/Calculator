67e7d922d1b73ced439de7a97199c4ff76730a05
Salmaan Saeed <salsaeed999@gmail.com>
2022-07-02 14:27:54 -0400

    attempting to add calculator functionality

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,21 @@ use std::rc::Rc;
 use std::cell::Cell;
 use gtk4 as gtk;
 use gtk::prelude::*;
-use gtk::{Application, Button, ApplicationWindow, Orientation};
+use gtk::{Application, Button, ApplicationWindow, Orientation, Entry};
 use glib;
 use glib_macros::clone;
 
 const APP_ID: &str = "org.gtk_rs.GObjectSubclassing1";
+
+struct Nums {
+    num1: i32,
+}
+
+impl Nums {
+    fn get(&self) -> i32 {
+        return self.num1;
+    }
+}
 
 fn main() {
     // Create a new application
@@ -32,15 +42,43 @@ fn build_ui(application: &Application) {
     let margin: i32 = 12;
 
     // Create two buttons
-    let button_increase = Button::builder()
-        .label("Increase")
+    let button_num1 = Button::builder()
+        .label("1")
         .margin_top(margin)
         .margin_bottom(margin)
         .margin_start(margin)
         .margin_end(margin)
         .build();
-    let button_decrease = Button::builder()
-        .label("Decrease")
+    let button_num2 = Button::builder()
+        .label("2")
+        .margin_top(margin)
+        .margin_bottom(margin)
+        .margin_start(margin)
+        .margin_end(margin)
+        .build();
+    let button_plus = Button::builder()
+        .label("+")
+        .margin_top(margin)
+        .margin_bottom(margin)
+        .margin_start(margin)
+        .margin_end(margin)
+        .build();
+    let button_minus = Button::builder()
+        .label("-")
+        .margin_top(margin)
+        .margin_bottom(margin)
+        .margin_start(margin)
+        .margin_end(margin)
+        .build();
+    let button_equals = Button::builder()
+        .label("=")
+        .margin_top(margin)
+        .margin_bottom(margin)
+        .margin_start(margin)
+        .margin_end(margin)
+        .build();
+    let button_clear = Button::builder()
+        .label("clear")
         .margin_top(margin)
         .margin_bottom(margin)
         .margin_start(margin)
@@ -54,23 +92,86 @@ fn build_ui(application: &Application) {
         .margin_end(margin)
         .build();
 
+    //let entry = Entry::new()
+
+   
+
     // A mutable integer
-    //let mut number = 0;
-    let number = Rc::new(Cell::new(0));
+    let num1: Rc<Cell<i64>> = Rc::new(Cell::new(0));
+    let num2: Rc<Cell<i64>> = Rc::new(Cell::new(0));
+    let ops = Rc::new(Cell::new('n'));
+    let num_counter = Rc::new(Cell::new(0));
+
+    //let rnums = Box::new(&mut nums);
+
 
     // Connect callbacks
     // When a button is clicked, `number` should be changed
-    button_increase.connect_clicked(clone!(@weak number, @weak button_increase, @weak button_show =>
+    button_num1.connect_clicked(clone!(@weak num1, @weak num2, @weak num_counter, @weak button_show =>
         move |_| {
-                number.set(number.get() + 1);
-                button_show.set_label(&number.get().to_string());
+            if num_counter.get() == 0 {
+                num1.set(num1.get() * 10 + 1);
+            }
+            if num_counter.get() == 0 {
+                num2.set(num2.get() * 10 + 1);
+            }
+            button_show.set_label(&num1.get().to_string());
 
         }));
 
-    button_decrease.connect_clicked(clone!(@weak button_decrease, @weak button_show =>
+    button_num2.connect_clicked(clone!(@weak num1, @weak num2, @weak num_counter, @weak button_show =>
         move |_| {
-                number.set(number.get() - 1);
-                button_show.set_label(&number.get().to_string());
+            if num_counter.get() == 0 {
+                num1.set(num1.get() * 10 + 2);
+            }
+            if num_counter.get() == 0 {
+                num2.set(num2.get() * 10 + 2);
+            }
+            button_show.set_label(&num2.get().to_string());
+
+        }));
+
+    button_plus.connect_clicked(clone!(@weak num1, @weak num2, @weak ops, @weak button_show, @weak num_counter=>
+        move |_| {
+            if num_counter.get() == 0 {
+                ops.set('+');
+            }
+            if num_counter.get() == 0 {
+                ops.set('+');
+            }
+            button_show.set_label(&ops.get().to_string());
+
+        }));
+    button_minus.connect_clicked(clone!(@weak num1, @weak num2, @weak ops, @weak button_show, @weak num_counter =>
+        move |_| {
+            if num_counter.get() == 0 {
+                ops.set('-');
+            }
+            if num_counter.get() == 0 {
+                ops.set('-');
+            }
+            button_show.set_label(&ops.get().to_string());
+    
+        }));
+    
+    button_equals.connect_clicked(clone!(@weak ops, @weak button_show =>
+        move |_| {
+            if num_counter.get() == 0 {
+                ops.set('=');
+            }
+            if num_counter.get() == 0 {
+                ops.set('=');
+            }
+            button_show.set_label(&ops.get().to_string());
+        
+        }));
+
+    button_clear.connect_clicked(clone!(@weak button_show =>
+        move |_| {
+            num1.set(0);
+            num2.set(0);
+            ops.set(' ');
+            button_show.set_label("");
         }));
 
     // Add buttons to `gtk_box`
@@ -79,8 +180,12 @@ fn build_ui(application: &Application) {
         .build();
 
     gtk_box.append(&button_show);
-    gtk_box.append(&button_increase);
-    gtk_box.append(&button_decrease);
+    gtk_box.append(&button_num1);
+    gtk_box.append(&button_num2);
+    gtk_box.append(&button_plus);
+    gtk_box.append(&button_minus);
+    gtk_box.append(&button_equals);
+    gtk_box.append(&button_clear);
 
     // Create a window
     let window = ApplicationWindow::builder()
