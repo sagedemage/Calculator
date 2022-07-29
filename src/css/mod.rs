c23e18ad31be778db5165e23050f4cb6cbadaa6b
Salmaan Saeed <salsaeed999@gmail.com>
2022-07-28 22:26:33 -0400

    modified the load_css_file function to load the css file from a path

--- src/css/mod.rs
+++ src/css/mod.rs
@@ -1,17 +1,22 @@
 /* Setup the CSS to improve the app appearance */
 
 use gdk::Display;
-use gtk::{CssProvider, StyleContext};
+use gtk::{CssProvider, StyleContext, STYLE_PROVIDER_PRIORITY_APPLICATION};
 
-pub fn load_css_file() {
-    // Load the CSS file and add it to the provider
+// resources/styles.css
+pub fn load_css_file(css_file_path: &str) {
+    /* Load the CSS file to change the 
+     * appearance of the app */
+    // CssProvider
     let provider = CssProvider::new();
-    provider.load_from_data(include_bytes!("style.css"));
+
+    // Load the CSS file
+    provider.load_from_path(css_file_path);
 
     StyleContext::add_provider_for_display(
         &Display::default().expect("Could not connect to a display."),
         &provider,
-        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        STYLE_PROVIDER_PRIORITY_APPLICATION,
     );
 }
 
--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,14 @@ pub use crate::ui::build_ui;
 pub use crate::css::load_css_file;
 
 const APP_ID: &str = "org.gtk_rs.GObjectSubclassing1";
+const CSS_FILE_PATH: &str = "src/resources/styles.css";
 
 fn main() {
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
     
     // Connect to signals
-    app.connect_startup(|_| load_css_file());
+    app.connect_startup(|_| load_css_file(CSS_FILE_PATH));
     app.connect_activate(build_ui);
 
     // Run the application
