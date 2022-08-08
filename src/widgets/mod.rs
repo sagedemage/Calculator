8f93e5cda11ffaccb5b6f6ab5042dec4fd91c295
Salmaan Saeed <salsaeed999@gmail.com>
2022-08-08 12:55:28 -0400

    added the negative sign button that does not do anything

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -30,16 +30,16 @@ pub struct NumberButtons {
 impl NumberButtons {
     pub fn new() -> NumberButtons {
         NumberButtons{
-            num0: create_button("0"),
-            num1: create_button("1"),
-            num2: create_button("2"),
-            num3: create_button("3"),
-            num4: create_button("4"),
-            num5: create_button("5"),
-            num6: create_button("6"),
-            num7: create_button("7"),
-            num8: create_button("8"),
-            num9: create_button("9"),
+            num0: create_button(" 0 "),
+            num1: create_button(" 1 "),
+            num2: create_button(" 2 "),
+            num3: create_button(" 3 "),
+            num4: create_button(" 4 "),
+            num5: create_button(" 5 "),
+            num6: create_button(" 6 "),
+            num7: create_button(" 7 "),
+            num8: create_button(" 8 "),
+            num9: create_button(" 9 "),
         }
     }
 } 
@@ -66,14 +66,16 @@ pub struct SpecialButtons {
     pub clear: Button,
     pub equals: Button,
     pub period: Button,
+    pub negative: Button,
 }
 
 impl SpecialButtons {
     pub fn new() -> SpecialButtons {
         SpecialButtons{
             clear: create_button("clear"),
-            equals: create_button("="),
-            period: create_button("."),
+            equals: create_button(" = "),
+            period: create_button(" . "),
+            negative: create_button("-/+"),
         }
     }
 }
